439680d684cccabc3e1e89b34cbd5b1e2c92182a
Nicholas Tay <nkt@outlook.kr>
2018-07-18 19:22:01 +1000

    Use a more algo based approach for maybe other advanced features

--- kr-type.js
+++ kr-type.js
@@ -1,82 +1,97 @@
 // ------------------------------------------------------------------------
 // Data - For now, let's just do the green highlighted ones on wikipedia
 // Other ones probably not gonna encounter lol
+// List the compatability then we can convert to this form.
 var HANGUL_MAPPINGS = {
-	'4352': 'r', // ㄱ
-	'4353': 'R', // ㄲ
-	'4354': 's', // ㄴ
-	'4355': 'e', // ㄷ
-	'4356': 'E', // ㄸ
-	'4357': 'f', // ㄹ
-	'4358': 'a', // ㅁ
-	'4359': 'q', // ㅂ
-	'4360': 'Q', // ㅃ
-	'4361': 't', // ㅅ
-	'4362': 'T', // ㅆ
-	'4363': 'd', // ㅇ
-	'4364': 'w', // ㅈ
-	'4365': 'W', // ㅉ
-	'4366': 'c', // ㅊ
-	'4367': 'z', // ㅋ
-	'4368': 'x', // ㅌ
-	'4369': 'v', // ㅍ
-	'4370': 'g', // ㅎ
+	// compatibility block
+	'12593': 'r', // ㄱ 
+	'12594': 'R', // ㄲ
+	'12595': 'rt', // ㄳ
+	'12596': 's', // ㄴ
+	'12597': 'sw', // ㄵ
+	'12598': 'sg', // ㄶ
+	'12599': 'e', // ㄷ
+	'12600': 'E', // ㄸ
+	'12601': 'f', // ㄹ
+	'12602': 'fr', // ㄺ
+	'12603': 'fa', // ㄻ
+	'12604': 'fq', // ㄼ
+	'12605': 'ft', // ㄽ
+	'12606': 'fx', // ㄾ
+	'12607': 'fv', // ㄿ
+	'12608': 'fg', // ㅀ
+	'12609': 'a', // ㅁ
+	'12610': 'q', // ㅂ
+	'12611': 'Q', // ㅃ
+	'12612': 'qt', // ㅄ
+	'12613': 't', // ㅅ
+	'12614': 'T', // ㅆ
+	'12615': 'd', // ㅇ
+	'12616': 'w', // ㅈ
+	'12617': 'W', // ㅉ
+	'12618': 'c', // ㅊ
+	'12619': 'z', // ㅋ
+	'12620': 'x', // ㅌ
+	'12621': 'v', // ㅍ
+	'12622': 'g', // ㅎ
+	'12623': 'k', // ㅏ
+	'12624': 'o', // ㅐ
+	'12625': 'i', // ㅑ
+	'12626': 'o', // ㅐ
+	'12627': 'j', // ㅓ
+	'12628': 'p', // ㅔ
+	'12629': 'u', // ㅕ
+	'12630': 'P', // ㅖ
+	'12631': 'h', // ㅗ
+	'12632': 'hk', // ㅘ
+	'12633': 'ho', // ㅙ
+	'12634': 'hl', // ㅚ
+	'12635': 'y', // ㅛ
+	'12636': 'n', // ㅜ
+	'12637': 'nj', // ㅝ
+	'12638': 'np', // ㅞ
+	'12639': 'nl', // ㅟ
+	'12640': 'b', // ㅠ
+	'12641': 'm', // ㅡ
+	'12642': 'ml', // ㅢ
+	'12643': 'l'  // ㅣ
+};
 
-	'4449': 'k', // ㅏ
-	'4450': 'o', // ㅐ
-	'4451': 'i', // ㅑ
-	'4452': 'O', // ㅒ
-	'4453': 'j', // ㅓ
-	'4454': 'p', // ㅔ
-	'4455': 'u', // ㅕ
-	'4456': 'P', // ㅖ
-	'4457': 'h', // ㅗ
-	'4458': 'hk', // ㅘ
-	'4459': 'ho', // ㅙ
-	'4460': 'hl', // ㅚ
-	'4461': 'y', // ㅛ
-	'4462': 'n', // ㅜ
-	'4463': 'nj', // ㅝ
-	'4464': 'np', // ㅞ
-	'4465': 'nl', // ㅟ
-	'4466': 'b', // ㅠ
-	'4467': 'm', // ㅡ
-	'4468': 'ml', // ㅢ
-	'4469': 'l', // ㅣ
+var SYLLABLE_COMPAT_MAPPING = {
+	// first bit (initial)
+	// the very start is a cbs - just gonna map manual
+	// but later 4361-4370 = 12613-12622
+	'4352': '12593', // ㄱ
+	'4353': '12594', // ㄲ
+	'4354': '12596', // ㄴ
+	'4355': '12599', // ㄷ
+	'4356': '12600', // ㄸ
+	'4357': '12601', // ㄹ
+	'4358': '12609', // ㅁ
+	'4359': '12610', // ㅂ
+	'4360': '12611'  // ㅃ
 
-	'4520': 'r', // ㄱ
-	'4521': 'R', // ㄲ
-	'4522': 'rt', // ㄳ
-	'4523': 's', // ㄴ
-	'4524': 'sw', // ㄵ
-	'4525': 'sg', // ㄶ
-	'4526': 'e', // ㄷ
-	'4527': 'f', // ㄹ
-	'4528': 'fr', // ㄺ
-	'4529': 'fa', // ㄻ
-	'4530': 'fq', // ㄼ
-	'4531': 'ft', // ㄽ
-	'4532': 'fx', // ㄾ
-	'4533': 'fv', // ㄿ
-	'4534': 'fg', // ㅀ
-	'4535': 'a', // ㅁ
-	'4536': 'q', // ㅂ
-	'4537': 'qt', // ㅄ
-	'4538': 't', // ㅅ
-	'4539': 'T', // ㅆ
-	'4540': 'd', // ㅇ
-	'4541': 'w', // ㅈ
-	'4542': 'c', // ㅊ
-	'4543': 'z', // ㅋ
-	'4544': 'x', // ㅌ
-	'4545': 'v', // ㅍ
-	'4546': 'g'  // ㅎ
-}
+	// second bit (medial)
+	// the medials actually line up. so lets use an algo
+	// 4449-4469 = 12623-12643
+	// therefore: 
+	//               medial + 8174 = compat
+
+	// third bit (final)
+	// theres sort of an algo here too
+	//
+	// 4520-4526 = 12593-12599
+	// 4527-4536 = 12601-12602
+	// 4537-4541 = 12612-12616
+	// 4542-4546 = 12618-12622
+	// so i guess when we reach these "checkpoint" numbers, we can add one.
+};
 // ------------------------------------------------------------------------
 
 function convert() {
 	var input = document.getElementById('input').value;
 	var inputN = input.normalize('NFD'); // normalize to destructured unicode
+
 	var output = '';
 	for (var i = 0; i < inputN.length; ++i) {
 		var c = inputN[i]; // the character in question
@@ -84,21 +99,48 @@ function convert() {
 
 		// are we hangul? if not, lets get out
 		if (!(
-			(cc >= 4352 && cc <= 4370) || // \u1100 - \u1112 = 1st bit, conso
-			(cc >= 4449 && cc <= 4469) || // \u1161 - \u1175 = 2nd bit, vowel
-			(cc >= 4520 && cc <= 4546)    // \u11A8 - \u11C2 = 3rd bit, conso
+			(cc >= 4352 && cc <= 4370) || // \u1100 - \u1112 = initial
+			(cc >= 4449 && cc <= 4469) || // \u1161 - \u1175 = medial
+			(cc >= 4520 && cc <= 4546) || // \u11A8 - \u11C2 = final
+			(cc >= 12593 && cc <= 12643)  // \u3131 - \u3163 = compat block
 		)) {
 			output += c;
 			continue;
 		}
 
-		output += HANGUL_MAPPINGS[cc];
+
+		// compat itself
+		if (cc >= 12593 && cc <= 12643) {
+			output += HANGUL_MAPPINGS[cc];
+		}
+
+		// initial
+		else if (cc >= 4352 && cc <= 4370) {
+			if (cc < 4361) {
+				output += HANGUL_MAPPINGS[SYLLABLE_COMPAT_MAPPING[cc]];
+			} else {
+				output += HANGUL_MAPPINGS[cc + 8252];
+			}
+		}
+
+		// medial
+		else if (cc >= 4449 && cc <= 4469) { 
+			output += HANGUL_MAPPINGS[cc + 8174];
+		}
+
+		// final
+		else {
+			var incr = 0;
+			if (cc > 4527)
+				++incr;
+			if (cc > 4537)
+				++incr;
+			if (cc > 4542)
+				++incr;
+
+			output += HANGUL_MAPPINGS[cc + incr + 8073];
+		}
 	}
 
 	document.getElementById('output').value = output;
-}
-
-function inkey() {
-	if (event.keyCode === 13) // enter
-		convert();
 }
